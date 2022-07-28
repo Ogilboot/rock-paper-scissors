b681fc6ede48cabf4a26afc99973cb262d7ee46c
Ogilboot <callum_ogilvy@hotmail.co.uk>
2022-07-28 12:09:00 +0000

    feat: computer choice and display function works

--- app.js
+++ app.js
@@ -5,5 +5,24 @@ const possibleChoices = document.querySelectorAll('button')
 let userChoice
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-    userChoiuce = e.target.id
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+/* generates random computer choice and displays this choice */
+function generateComputerChoice () {
+    const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1
+
+    if (randomNumber === 1) {
+        computerChoice = 'rock'
+    } 
+    else if (randomNumber === 2) {
+        computerChoice = 'scissors'
+    }
+    else if (randomNumber === 3) {
+        computerChoice = 'paper'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+}
